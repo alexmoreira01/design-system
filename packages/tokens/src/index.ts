90a0e597d04d83399e26571ca17c6e36073e39e0
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-06-21 23:23:47 -0300

    Feature: init package tokens

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,5 @@
+export * from './colors';
+
+/* Quando for utilizar */
+// import { colors } from '@ignite-ui/tokens/colors' -- Ao invez de se utilizar assim 
+// import { colors } from '@ignite-ui/tokens' -- Podera se utilizar assim
