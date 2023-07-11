1d2c05383d37d846eca8cf6648a7d6aa02320f6a
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-07-10 22:53:24 -0300

    Feature: Create components and install turborepo

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non quos, quod doloremque animi ipsam asperiores eaque repellat saepe nam. Aliquam perferendis quam eaque doloremque, dolor porro reiciendis illo? Totam, autem!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
