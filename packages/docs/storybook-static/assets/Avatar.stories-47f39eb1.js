3b0c8e20e1fabccf424aef3a266c688f565f6be1
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-07-15 23:32:08 -0300

    Feat: add build

---
+++ packages/docs/storybook-static/assets/Avatar.stories-47f39eb1.js
@@ -0,0 +1,6 @@
+import{A as m}from"./index-df802303.js";import"./index-c0290abd.js";import"./_commonjsHelpers-725317a4.js";import"./extends-98964cd2.js";import"./jsx-runtime-5fc188ad.js";const g={title:"Data display/Avatar",component:m,args:{src:"https://github.com/alexmoreira01.png",alt:"Alex Moreira"}},r={},a={args:{src:void 0}};var e,s,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var o,c,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    src: undefined
+  }
+}`,...(i=(c=a.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const A=["Primary","WithFallback"];export{r as Primary,a as WithFallback,A as __namedExportsOrder,g as default};
+//# sourceMappingURL=Avatar.stories-47f39eb1.js.map
---
+++ packages/docs/storybook-static/assets/syntaxhighlighter-QTQ2UBB4-1199de0c.js
@@ -0,0 +1,2 @@
+import{S as d,c as f,s as g}from"./chunk-PCJTTTQV-426d2ea0.js";import"./iframe-c344f0c6.js";import"../sb-preview/runtime.js";import"./index-c0290abd.js";import"./_commonjsHelpers-725317a4.js";import"./index-e8a4bda7.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./index-356e4a49.js";export{d as SyntaxHighlighter,f as createCopyToClipboardFunction,g as default};
+//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-1199de0c.js.map
